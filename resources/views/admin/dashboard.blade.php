b5a997291fe44c09f6e9adb8bf8c29504707c350
Joel Solera <joelss084@gmail.com>
2023-05-31 23:46:01 -0500

    add admin view

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,7 @@
 @extends('admin.layouts.template')
+@section('page_title')
+Dashboard - Ecommerce
+@endsection
 @section('content')
 Hellow From Dashboard
 @endsection
--- resources/views/admin/pendingorders.blade.php
+++ resources/views/admin/pendingorders.blade.php
@@ -1,4 +1,7 @@
 @extends('admin.layouts.template')
+@section('page_title')
+Pending Orders - Ecommerce
+@endsection
 @section('content')
 Hellow From pending orders
 @endsection
