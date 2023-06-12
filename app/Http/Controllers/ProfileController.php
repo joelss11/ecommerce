48295835065caca5dbd21ef8d1e745d5be130883
Joel Solera <joelss084@gmail.com>
2023-06-11 20:34:54 -0500

    profile added

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -37,7 +37,7 @@ class ProfileController extends Controller
 
         $request->user()->save();
 
-        return Redirect::route('profile.edit')->with('status', 'profile-updated');
+        return Redirect::route('admindashboard')->with('status', 'profile-updated');
     }
 
     /**
--- resources/views/admin/layouts/template.blade.php
+++ resources/views/admin/layouts/template.blade.php
@@ -183,7 +183,7 @@
                                         <div class="dropdown-divider"></div>
                                     </li>
                                     <li>
-                                        <a class="dropdown-item" href="{{ route('profile.edit') }}">
+                                        <a class="dropdown-item" href="{{ route('admindashboard') }}">
                                             <i class="bx bx-user me-2"></i>
                                             <span class="align-middle">My Profile</span>
                                         </a>
